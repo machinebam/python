9bd01b94f5468ff3e67278944665d50899db5b13
Bam <scamula@live.com>
2014-11-22 15:57:18 +0800

    First commit
    Comi

---
+++ rock.py
@@ -0,0 +1,24 @@
+import time
+from twython import TwythonStreamer
+import os
+
+# Search terms
+TERMS = '#yes'
+
+
+
+
+# Setup callbacks from Twython Streamer
+class BlinkyStreamer(TwythonStreamer):
+        def on_success(self, data):
+                if 'text' in data:
+                        print data['text'].encode('utf-8')
+                        os.system('omxplayer smell.wav')
+                     
+
+# Create streamer
+try:
+        stream = BlinkyStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+        stream.statuses.filter(track=TERMS)
+except KeyboardInterrupt:
+        GPIO.cleanup()
